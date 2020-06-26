4260c98c124779a5322e04a8cf9825f6046c80aa
Swathi Mukkamala <swathikmukkamala@gmail.com>
2020-06-26 16:05:19 -0400

    made a proxy server to manage Directions API

---
+++ server.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 6060
+app.set('port', port)
+const request = require('request');
+
+// MIDDLEWARE (transform stream)
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
+
+app.get('/api/directions/*', (req, res) => {
+  let apiCall = req.url.slice('/api/directions/'.length)
+
+  let apiReq = `https://api.mapbox.com/directions/v5/mapbox/driving/${apiCall}`
+
+  request.get(apiReq, (err, _, body) => {
+    res.send(body)
+  });
+});
+
+// app.get('/made/up/path/*', (req, res) => {
+//     let apiCall = req.url.slice('/made/up/path/'.length)
+//     let apiReq = `externalAPI${apiCall}`
+//     request.get(apiReq, (err, _, body) => {
+//       res.send(body)
+//     });
+
+app.listen(port, () =>
+  console.log(`Listening on port: ${port}`)
+)
